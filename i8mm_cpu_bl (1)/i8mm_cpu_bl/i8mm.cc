822fc49cbd8f8ebf85445ad8067636b5d0f00a8b
peepeepoopooooo <phantienminhthuyy@gmail.com>
2026-02-12 05:47:45 +0200

    i8mm.cc: added int tilesize, added int kk_tileend to account for partial tiles, added acc to account for accumulation of the sum product to meet no zero matrix C initiation requirement of the test, initiate matrix C only at the beginning as the first iterated accumulation. int32_t acc = 0 added. i8mm.cc: passed all unit matrix tests and benchmarks at --no-timeout.

--- i8mm_cpu_bl (1)/i8mm_cpu_bl/i8mm.cc
+++ i8mm_cpu_bl (1)/i8mm_cpu_bl/i8mm.cc
@@ -1,26 +1,37 @@
 #include "mm.h"
+#include <algorithm>
+#include <cstdint> 
+void gemm(int m, int n, int k,
+          const std::int8_t* A,
+          const std::int8_t* B,
+          std::int32_t* C)
+{
+    const int tilesize = 64;
+    
+    for (int kk = 0; kk < k ; kk += tilesize) {
+        for (int i = 0; i < m ; ++i) {
+            int kk_tileend = std::min(k, kk + tilesize); 
+            for (int j =0; j < n; ++j){
+                int32_t acc = 0; 
+                for (int k_inner = kk; k_inner < kk_tileend ; ++k_inner){
+                    acc += A[i*k+ k_inner]*B[k_inner*n+j]; 
+                }
 
-void gemm ( int m , int n , int k ,
-const std :: int8_t A , const std :: int8_t B , std :: int32_t C ) {
-    for (int i = 0; i < m ; ++ i) {
-        for (int ij = 0, ij < jc, ++ij){
+                if (kk == 0) {
+                    C[i*n + j] = acc;
+                }
+                else         {
+                    C[i*n + j] += acc;
+                }
+
+             
 
          
-            for (int kk = 0; kk < k ; ++ kk ) {
-                for (int kj = 0, kj < kc, ++ij){
-
-
-                    
-                    for (int j = 0; j < n ; ++ j ) {
-                        for(int jj = 0, jj < jc, ++jj ){
-                            C[i*n+j] += A[i*k+kk]*B[kk*n+j];
-
-                }
-            } 
-                }
+            
+               
             }
         }
-    } 
+    }
 }
 
-
+ 
